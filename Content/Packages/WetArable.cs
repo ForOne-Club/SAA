c21fabd850e3ec364bdc1563efe419b854006521
ReishyouSose <1657188940@qq.com>
2024-01-14 20:19:03 +0800

    ？

--- Content/Packages/WetArable.cs
+++ Content/Packages/WetArable.cs
@@ -1,12 +1,32 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using NetSimplified;
+using NetSimplified.Syncing;
+using static SAA.Content.Planting.System.PlowlandSystem;
 
 namespace SAA.Content.Packages
 {
-    internal class WetArable
+    [AutoSync]
+    public class WetArable : NetModule
     {
+        private int tileX;
+        private int tileY;
+        public static void Send(int tileX, int tileY)
+        {
+            var p = NetModuleLoader.Get<WetArable>();
+            p.tileX = tileX;
+            p.tileY = tileY;
+            p.Send();
+        }
+        public override void Receive()
+        {
+            if (Main.dedServ)
+            {
+                Send(-1, Sender);
+            }
+            if (wet.Contains((tileX, tileY)))
+            {
+                wet.Remove((tileX, tileY));
+            }
+            else wet.Add((tileX, tileY));
+        }
     }
 }
