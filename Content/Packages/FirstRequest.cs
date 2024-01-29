b6506b458772e43f2fa2cd19ef7ad7d6b43c8ed0
泠潇丶苏ゆき <1657188940@qq.com>
2024-01-29 21:41:16 +0800

    这什么莫名其妙的？

--- Content/Packages/FirstRequest.cs
+++ Content/Packages/FirstRequest.cs
@@ -10,6 +10,7 @@ namespace SAA.Content.Packages
         {
             if (Main.dedServ)
             {
+                if (wet == null) return;
                 p.Write(wet.Count);
                 foreach (var (x, y) in wet)
                 {
